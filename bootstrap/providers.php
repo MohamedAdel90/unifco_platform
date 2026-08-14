6d3b19541272a138476b39e287821d7796c09eca
zeroonesolutions0101 <zeroone.solutions0101@gmail.com>
2026-08-15 01:14:34 +0300

    Register Wave 12 service provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,4 +5,5 @@ return [
     App\Providers\Wave9ServiceProvider::class,
     App\Providers\Wave10ServiceProvider::class,
     App\Providers\Wave11ServiceProvider::class,
+    App\Providers\Wave12ServiceProvider::class,
 ];
