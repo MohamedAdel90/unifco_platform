319c2d38f2e14554c0017651dd366e4980be9de4
zeroonesolutions0101 <zeroone.solutions0101@gmail.com>
2026-08-15 01:42:19 +0300

    Enable Wave 13 service provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -6,4 +6,5 @@ return [
     App\Providers\Wave10ServiceProvider::class,
     App\Providers\Wave11ServiceProvider::class,
     App\Providers\Wave12ServiceProvider::class,
+    App\Providers\Wave13ServiceProvider::class,
 ];
