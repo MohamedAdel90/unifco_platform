48f6d750f3f9e9026c728dbf984d13e6aee88620
zeroonesolutions0101 <zeroone.solutions0101@gmail.com>
2026-08-18 15:36:12 +0300

    Expose public request business pipeline links

--- app/Models/PublicServiceRequest.php
+++ app/Models/PublicServiceRequest.php
@@ -9,7 +9,9 @@ class PublicServiceRequest extends Model
     protected $fillable = [
         'reference_no','request_type','service_category','subject','details','site_city','urgency',
         'company_name','commercial_registration','email','mobile','status','submitted_at',
+        'tenant_id','organization_id','crm_lead_id','crm_opportunity_id','crm_quotation_id',
+        'service_request_id','work_order_id','converted_at',
     ];
 
-    protected $casts = ['submitted_at' => 'datetime'];
+    protected $casts = ['submitted_at' => 'datetime', 'converted_at' => 'datetime'];
 }
