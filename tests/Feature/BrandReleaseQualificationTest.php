c2b8f5d0202c747a5af06cc1ecadf4b5b6985056
zeroonesolutions0101 <zeroone.solutions0101@gmail.com>
2026-08-20 01:02:53 +0300

    Fix branding qualification for current UNIFCO palette

--- tests/Feature/BrandReleaseQualificationTest.php
+++ tests/Feature/BrandReleaseQualificationTest.php
@@ -34,15 +34,16 @@ class BrandReleaseQualificationTest extends TestCase
         $this->assertFileExists(public_path('images/unifco-logo.webp'));
     }
 
-    public function test_public_and_login_pages_use_the_official_logo_and_palette(): void
+    public function test_public_and_login_pages_use_the_official_logo_and_brand_palette(): void
     {
-        foreach (['/', '/login'] as $url) {
-            $this->get($url)
-                ->assertOk()
-                ->assertSee('/brand/unifco-logo.webp', false)
-                ->assertSee('#1e315b', false)
-                ->assertSee('#132137', false)
-                ->assertSee('#ce122d', false);
-        }
+        $public = $this->get('/');
+        $public->assertOk()->assertSee('/brand/unifco-logo.webp', false);
+
+        $login = $this->get('/login');
+        $login->assertOk()
+            ->assertSee('/brand/unifco-logo.webp', false)
+            ->assertSee('#06275c', false)
+            ->assertSee('#071f4d', false)
+            ->assertSee('#e20b24', false);
     }
 }
