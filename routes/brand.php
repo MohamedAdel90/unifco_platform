99a7c38b80a36da36f8e5e7f0b56270709e97e83
zeroonesolutions0101 <zeroone.solutions0101@gmail.com>
2026-08-21 17:36:50 +0300

    Bust cache for official UNIFCO logo

--- routes/brand.php
+++ routes/brand.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\BrandAssetController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/brand/unifco-logo-v2.webp', [BrandAssetController::class, 'logo'])->name('brand.logo');
+Route::get('/brand/unifco-logo-v3.webp', [BrandAssetController::class, 'logo'])->name('brand.logo');
