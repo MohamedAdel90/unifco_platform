ccabc6889eba2d4b7b38e689d2596e0abd59b67a
zeroonesolutions0101 <zeroone.solutions0101@gmail.com>
2026-08-22 21:02:37 +0300

    Add user administration CRUD routes

--- routes/navigation.php
+++ routes/navigation.php
@@ -1,8 +1,22 @@
 <?php
 
+use App\Http\Controllers\Admin\UserAdministrationController;
 use App\Http\Controllers\NavigationWorkspaceController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')->prefix('workspace')->name('workspace.')->group(function () {
-    Route::get('/{workspace}',[NavigationWorkspaceController::class,'show'])->name('show');
+Route::middleware('auth')->group(function () {
+    Route::prefix('workspace')->name('workspace.')->group(function () {
+        Route::get('/{workspace}',[NavigationWorkspaceController::class,'show'])->name('show');
+    });
+
+    Route::prefix('admin/users')->name('admin.users.')->group(function () {
+        Route::get('/create',[UserAdministrationController::class,'create'])->name('create');
+        Route::post('/',[UserAdministrationController::class,'store'])->name('store');
+        Route::get('/{user}',[UserAdministrationController::class,'show'])->name('show');
+        Route::get('/{user}/edit',[UserAdministrationController::class,'edit'])->name('edit');
+        Route::put('/{user}',[UserAdministrationController::class,'update'])->name('update');
+        Route::post('/{user}/status',[UserAdministrationController::class,'status'])->name('status');
+        Route::post('/{user}/reset-password',[UserAdministrationController::class,'resetPassword'])->name('reset-password');
+        Route::post('/{user}/permission',[UserAdministrationController::class,'permission'])->name('permission');
+    });
 });
