8fe630aa0a51c67b765180414901cc6888eedf37
zeroonesolutions0101 <zeroone.solutions0101@gmail.com>
2026-08-23 16:02:10 +0300

    Load HR Phase 8 self service routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
             __DIR__.'/../routes/wave12.php',
             __DIR__.'/../routes/wave13.php',
             __DIR__.'/../routes/wave14.php',
+            __DIR__.'/../routes/wave15.php',
             __DIR__.'/../routes/navigation.php',
         ],
         api: __DIR__.'/../routes/api.php',
