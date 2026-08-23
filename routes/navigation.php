8d687b2b622f0da6740bc97c99a77a3ad0840859
zeroonesolutions0101 <zeroone.solutions0101@gmail.com>
2026-08-23 13:35:36 +0300

    Add enterprise user administration routes

--- routes/navigation.php
+++ routes/navigation.php
@@ -8,14 +8,16 @@ Route::middleware('auth')->group(function () {
     Route::prefix('workspace')->name('workspace.')->group(function () {
         Route::get('/{workspace}',[NavigationWorkspaceController::class,'show'])->name('show');
     });
-
     Route::prefix('admin/users')->name('admin.users.')->group(function () {
+        Route::get('/export/csv',[UserAdministrationController::class,'export'])->name('export');
+        Route::post('/bulk',[UserAdministrationController::class,'bulk'])->name('bulk');
         Route::get('/create',[UserAdministrationController::class,'create'])->name('create');
         Route::post('/',[UserAdministrationController::class,'store'])->name('store');
         Route::get('/{user}',[UserAdministrationController::class,'show'])->name('show');
         Route::get('/{user}/edit',[UserAdministrationController::class,'edit'])->name('edit');
         Route::put('/{user}',[UserAdministrationController::class,'update'])->name('update');
         Route::post('/{user}/status',[UserAdministrationController::class,'status'])->name('status');
+        Route::post('/{user}/security',[UserAdministrationController::class,'security'])->name('security');
         Route::post('/{user}/reset-password',[UserAdministrationController::class,'resetPassword'])->name('reset-password');
         Route::post('/{user}/permission',[UserAdministrationController::class,'permission'])->name('permission');
     });
