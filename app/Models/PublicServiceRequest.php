f0064205576251ed7e0afac25febb223042bf2d7
zeroonesolutions0101 <zeroone.solutions0101@gmail.com>
2026-08-24 12:56:10 +0300

    Support richer public request intake data

--- app/Models/PublicServiceRequest.php
+++ app/Models/PublicServiceRequest.php
@@ -7,9 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class PublicServiceRequest extends Model
 {
     protected $fillable = [
-        'reference_no','request_type','service_category','subject','details','site_city','site_address','latitude','longitude','urgency',
-        'requested_date','requested_time','equipment_image_path','supporting_image_paths',
-        'company_name','responsible_person','commercial_registration','email','mobile','status','submitted_at',
+        'reference_no','request_type','request_intent','service_category','service_family','asset_type','subject','details','site_city','site_address','latitude','longitude','urgency',
+        'requested_date','requested_time','equipment_image_path','supporting_image_paths','supporting_document_paths',
+        'company_name','responsible_person','contact_role','commercial_registration','email','mobile','status','submitted_at',
         'tenant_id','organization_id','crm_lead_id','crm_opportunity_id','crm_quotation_id',
         'service_request_id','work_order_id','converted_at',
     ];
@@ -19,6 +19,7 @@ class PublicServiceRequest extends Model
         'converted_at' => 'datetime',
         'requested_date' => 'date',
         'supporting_image_paths' => 'array',
+        'supporting_document_paths' => 'array',
         'latitude' => 'decimal:7',
         'longitude' => 'decimal:7',
     ];
