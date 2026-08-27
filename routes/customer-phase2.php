0df4998962e49b94160b48a0ea77c6b9ec7eb3cd
zeroonesolutions0101 <zeroone.solutions0101@gmail.com>
2026-08-27 14:55:23 +0300

    Add internal customer action inbox routes

--- routes/customer-phase2.php
+++ routes/customer-phase2.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\{CustomerActionCenterController,CustomerPortalPhase2ActionController};
+use App\Http\Controllers\Workflow\CustomerActionInboxController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
@@ -9,4 +10,8 @@ Route::middleware('auth')->group(function () {
     Route::post('/customer/invoices/{invoice}/query', [CustomerPortalPhase2ActionController::class,'invoiceQuery'])->name('customer.invoices.query');
     Route::post('/customer/invoices/{invoice}/payment-proof', [CustomerPortalPhase2ActionController::class,'paymentProof'])->name('customer.invoices.payment-proof');
     Route::post('/customer/work-orders/{workOrder}/revisit', [CustomerPortalPhase2ActionController::class,'requestRevisit'])->name('customer.work-orders.revisit');
+
+    Route::get('/workflow/customer-actions', [CustomerActionInboxController::class,'index'])->name('workflow.customer-actions.index');
+    Route::post('/workflow/customer-actions/{action}/resolve', [CustomerActionInboxController::class,'resolve'])->name('workflow.customer-actions.resolve');
+    Route::get('/workflow/customer-actions/{action}/attachment', [CustomerActionInboxController::class,'attachment'])->name('workflow.customer-actions.attachment');
 });
