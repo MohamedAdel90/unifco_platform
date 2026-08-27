72fdc9b58feaee2ad0f89f7d510fab5d33d539e0
zeroonesolutions0101 <zeroone.solutions0101@gmail.com>
2026-08-27 21:38:23 +0300

    Register asset lifecycle and commissioning routes

--- routes/asset-master.php
+++ routes/asset-master.php
@@ -7,9 +7,14 @@ Route::middleware('auth')->prefix('asset-master')->name('asset-master.')->group(
     Route::get('/',[AssetMasterController::class,'index'])->name('index');
     Route::post('/',[AssetMasterController::class,'store'])->name('store');
     Route::post('/templates',[AssetMasterController::class,'storeTemplate'])->name('templates.store');
+    Route::post('/locations',[AssetMasterController::class,'storeLocation'])->name('locations.store');
     Route::get('/{asset}',[AssetMasterController::class,'show'])->name('show');
     Route::put('/{asset}',[AssetMasterController::class,'update'])->name('update');
     Route::post('/{asset}/verify',[AssetMasterController::class,'verify'])->name('verify');
+    Route::post('/{asset}/transition',[AssetMasterController::class,'transition'])->name('transition');
+    Route::post('/{asset}/assign-location',[AssetMasterController::class,'assignLocation'])->name('assign-location');
+    Route::post('/{asset}/commissioning',[AssetMasterController::class,'requestCommissioning'])->name('commissioning.request');
+    Route::post('/{asset}/commissioning/{record}/review',[AssetMasterController::class,'reviewCommissioning'])->name('commissioning.review');
     Route::post('/{asset}/documents',[AssetMasterController::class,'document'])->name('documents.store');
     Route::get('/documents/{document}/download',[AssetMasterController::class,'download'])->name('documents.download');
 });
