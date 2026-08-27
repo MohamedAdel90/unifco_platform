087cc78296dc98005b9424015e04d298f44b51a5
zeroonesolutions0101 <zeroone.solutions0101@gmail.com>
2026-08-27 14:43:44 +0300

    Add Phase 2 customer action routes

--- routes/customer-phase2.php
+++ routes/customer-phase2.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Controllers\CustomerActionCenterController;
+use App\Http\Controllers\{CustomerActionCenterController,CustomerPortalPhase2ActionController};
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
     Route::get('/customer/actions', CustomerActionCenterController::class)->name('customer.actions');
+    Route::post('/customer/contracts/{contract}/renewal-request', [CustomerPortalPhase2ActionController::class,'requestContractRenewal'])->name('customer.contracts.renewal-request');
+    Route::post('/customer/invoices/{invoice}/query', [CustomerPortalPhase2ActionController::class,'invoiceQuery'])->name('customer.invoices.query');
 });
