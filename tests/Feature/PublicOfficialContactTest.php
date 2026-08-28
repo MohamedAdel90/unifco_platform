edb9b3c7a9eb2ce66984eed50ffbb90dc1c4d3ef
zeroonesolutions0101 <zeroone.solutions0101@gmail.com>
2026-08-28 21:43:46 +0300

    Align homepage contact test with icon-only presentation

--- tests/Feature/PublicOfficialContactTest.php
+++ tests/Feature/PublicOfficialContactTest.php
@@ -16,14 +16,15 @@ class PublicOfficialContactTest extends TestCase
         $this->assertStringStartsWith('https://wa.me/966599402090',UnifcoContact::whatsappUrl('Test'));
     }
 
-    public function test_public_home_exposes_official_whatsapp_and_email(): void
+    public function test_public_home_exposes_icon_only_official_whatsapp_and_email_actions(): void
     {
         $this->get('/')
             ->assertOk()
-            ->assertSee('0599402090')
-            ->assertSee('info@unifco.com')
+            ->assertDontSee('0599402090')
             ->assertSee('https://wa.me/966599402090',false)
-            ->assertSee('mailto:info@unifco.com',false);
+            ->assertSee('mailto:info@unifco.com',false)
+            ->assertSee('unifco-nav-contact-menu',false)
+            ->assertSee('unifco-contact-icons',false);
     }
 
     public function test_public_request_page_exposes_same_official_contact_channels(): void
