2e89f937c2aa5440cd588c1cbd3935a5bfb6a2d6
zeroonesolutions0101 <zeroone.solutions0101@gmail.com>
2026-08-28 12:51:01 +0300

    Add purchase lifecycle and criticality acceptance fields

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -12,9 +12,10 @@ class Asset extends Model
 
     protected $fillable = [
         'tenant_id','organization_id','customer_id','customer_site_id','asset_location_id','parent_asset_id','asset_category_template_id','asset_code','customer_asset_code','name','location_code','serial_no','manufacturer_asset_number',
-        'asset_category','asset_subcategory','asset_type','manufacturer','model_no','criticality','ownership_type','lifecycle_status','operational_status','maintenance_strategy','pm_template','pm_frequency','last_pm','next_pm','last_inspection','next_inspection',
-        'building','floor','zone','room','room_code','physical_location','latitude','longitude','manufacture_date','installation_date','warranty_start','warranty_expiry','warranty_provider','contract_reference','sla_reference','coverage_type',
-        'technical_specifications','acquisition_cost','replacement_value','salvage_value','useful_life_months','expected_replacement_date','accumulated_depreciation','net_book_value',
+        'asset_category','asset_subcategory','asset_type','manufacturer','model_no','criticality','impact_safety','impact_operation','impact_financial','impact_customer','impact_environmental','probability_failure','probability_condition','probability_age','criticality_matrix_score','criticality_class',
+        'ownership_type','lifecycle_status','operational_status','maintenance_strategy','pm_template','pm_frequency','last_pm','next_pm','last_inspection','next_inspection',
+        'building','floor','zone','room','room_code','physical_location','latitude','longitude','manufacture_date','purchase_date','po_number','purchase_value','installation_date','warranty_start','warranty_expiry','warranty_provider','warranty_terms','contract_reference','sla_reference','coverage_type',
+        'technical_specifications','acquisition_cost','replacement_value','salvage_value','useful_life_months','expected_replacement_date','replacement_target_date','replacement_cost_estimate','accumulated_depreciation','net_book_value',
         'meter_value','operating_hours','meter_unit','design_capacity','current_load','failure_impact','commission_date','commissioning_status','commissioning_requested_by','commissioning_requested_at','commissioning_approved_by','commissioning_approved_at','commissioning_notes',
         'supplier_name','installer_name','qr_token','verification_status','data_completeness_score','verified_by','verified_at','verification_notes',
         'health_score','health_band','remaining_life_months','replacement_recommendation','replacement_reason','last_health_calculated_at','disposed_at','status'
@@ -23,9 +24,9 @@ class Asset extends Model
     protected function casts(): array
     {
         return [
-            'acquisition_cost'=>'decimal:2','replacement_value'=>'decimal:2','salvage_value'=>'decimal:2','accumulated_depreciation'=>'decimal:2','net_book_value'=>'decimal:2',
-            'meter_value'=>'decimal:4','operating_hours'=>'decimal:2','design_capacity'=>'decimal:3','current_load'=>'decimal:3','latitude'=>'decimal:7','longitude'=>'decimal:7','manufacture_date'=>'date','installation_date'=>'date','commission_date'=>'date',
-            'warranty_start'=>'date','warranty_expiry'=>'date','expected_replacement_date'=>'date','last_pm'=>'date','next_pm'=>'date','last_inspection'=>'date','next_inspection'=>'date','technical_specifications'=>'array','verified_at'=>'datetime',
+            'acquisition_cost'=>'decimal:2','purchase_value'=>'decimal:2','replacement_value'=>'decimal:2','replacement_cost_estimate'=>'decimal:2','salvage_value'=>'decimal:2','accumulated_depreciation'=>'decimal:2','net_book_value'=>'decimal:2','criticality_matrix_score'=>'decimal:2',
+            'meter_value'=>'decimal:4','operating_hours'=>'decimal:2','design_capacity'=>'decimal:3','current_load'=>'decimal:3','latitude'=>'decimal:7','longitude'=>'decimal:7','manufacture_date'=>'date','purchase_date'=>'date','installation_date'=>'date','commission_date'=>'date',
+            'warranty_start'=>'date','warranty_expiry'=>'date','expected_replacement_date'=>'date','replacement_target_date'=>'date','last_pm'=>'date','next_pm'=>'date','last_inspection'=>'date','next_inspection'=>'date','technical_specifications'=>'array','verified_at'=>'datetime',
             'commissioning_requested_at'=>'datetime','commissioning_approved_at'=>'datetime','last_health_calculated_at'=>'datetime','disposed_at'=>'datetime'
         ];
     }
