0f63c5a1aafb7906dfb190e80b29bc3f28566e1b
MohamedAdel90 <adelwork90@gmail.com>
2026-09-02 15:54:39 +0300

    feat(home): enable live asset and site counters

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Middleware\{AuthenticateApiToken,AuthenticateJwt,BrandingPresentation,CustomerPortalDashboardPresentation,CustomerPortalMobileLogoutPresentation,EnsureUserSessionValid,LegacyFormCompatibility,PublicAssetQrInsecureFallback,PublicAssetQrPresentation,PublicContactPresentation,PublicHomeFooterPresentation,PublicHomeOperationsPresentation,PublicHomeSocialLinksPresentation,PublicHomeSpacingPresentation,PublicRequestAttachmentsPresentation,PublicRequestCompactDesign,PublicRequestHeaderMatch,PublicServiceLinks,RequirePermission,WorkflowRoleHomeRedirect,WorkflowRoleNavigationPresentation};
+use App\Http\Middleware\{AuthenticateApiToken,AuthenticateJwt,BrandingPresentation,CustomerPortalDashboardPresentation,CustomerPortalMobileLogoutPresentation,EnsureUserSessionValid,LegacyFormCompatibility,PublicAssetQrInsecureFallback,PublicAssetQrPresentation,PublicContactPresentation,PublicHomeAssetStatsPresentation,PublicHomeFooterPresentation,PublicHomeOperationsPresentation,PublicHomeSocialLinksPresentation,PublicHomeSpacingPresentation,PublicRequestAttachmentsPresentation,PublicRequestCompactDesign,PublicRequestHeaderMatch,PublicServiceLinks,RequirePermission,WorkflowRoleHomeRedirect,WorkflowRoleNavigationPresentation};
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -37,7 +37,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias(['permission'=>RequirePermission::class,'api.token'=>AuthenticateApiToken::class,'jwt'=>AuthenticateJwt::class]);
-        $middleware->web(append: [EnsureUserSessionValid::class,WorkflowRoleHomeRedirect::class,WorkflowRoleNavigationPresentation::class,LegacyFormCompatibility::class,CustomerPortalDashboardPresentation::class,CustomerPortalMobileLogoutPresentation::class,PublicHomeSocialLinksPresentation::class,PublicHomeSpacingPresentation::class,PublicHomeFooterPresentation::class,PublicHomeOperationsPresentation::class,BrandingPresentation::class,PublicRequestHeaderMatch::class,PublicRequestAttachmentsPresentation::class,PublicAssetQrInsecureFallback::class,PublicAssetQrPresentation::class,PublicRequestCompactDesign::class,PublicServiceLinks::class,PublicContactPresentation::class]);
+        $middleware->web(append: [EnsureUserSessionValid::class,WorkflowRoleHomeRedirect::class,WorkflowRoleNavigationPresentation::class,LegacyFormCompatibility::class,CustomerPortalDashboardPresentation::class,CustomerPortalMobileLogoutPresentation::class,PublicHomeAssetStatsPresentation::class,PublicHomeSocialLinksPresentation::class,PublicHomeSpacingPresentation::class,PublicHomeFooterPresentation::class,PublicHomeOperationsPresentation::class,BrandingPresentation::class,PublicRequestHeaderMatch::class,PublicRequestAttachmentsPresentation::class,PublicAssetQrInsecureFallback::class,PublicAssetQrPresentation::class,PublicRequestCompactDesign::class,PublicServiceLinks::class,PublicContactPresentation::class]);
         $middleware->validateCsrfTokens(except: ['login','service-requests']);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
