482a44da552e3bb464af2f99b394745ade978bae
MohamedAdel90 <adelwork90@gmail.com>
2026-09-03 21:10:07 +0300

    Apply current maintenance form enhancements

--- routes/current-customer-maintenance.php
+++ routes/current-customer-maintenance.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\CurrentCustomerMaintenanceRequestController;
+use App\Http\Middleware\PublicCurrentMaintenanceFormEnhancements;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/request-service/current-maintenance', [CurrentCustomerMaintenanceRequestController::class, 'create'])->name('public.current-maintenance');
+Route::get('/request-service/current-maintenance', [CurrentCustomerMaintenanceRequestController::class, 'create'])
+    ->middleware(PublicCurrentMaintenanceFormEnhancements::class)
+    ->name('public.current-maintenance');
 Route::get('/request-service/current-maintenance/customer', [CurrentCustomerMaintenanceRequestController::class, 'customer'])->middleware('throttle:30,1')->name('public.current-maintenance.customer');
 Route::get('/request-service/current-maintenance/assets', [CurrentCustomerMaintenanceRequestController::class, 'assets'])->middleware('throttle:60,1')->name('public.current-maintenance.assets');
