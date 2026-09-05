47ddcdd4f130ac0ef8364609d961a7da1aab7033
MohamedAdel90 <adelwork90@gmail.com>
2026-09-05 22:36:33 +0300

    Add section display modes to CMS schema

--- app/Services/HomepageSectionSchema.php
+++ app/Services/HomepageSectionSchema.php
@@ -8,19 +8,19 @@ class HomepageSectionSchema
     {
         return match ($sectionKey) {
             'hero' => [
-                'scalars' => ['render_mode', 'image', 'eyebrow', 'title', 'text', 'button'],
+                'scalars' => ['display_mode', 'full_section_image', 'image', 'eyebrow', 'title', 'text', 'button'],
                 'items' => [
                     'proofs' => ['icon', 'label', 'sub'],
                 ],
             ],
             'capabilities' => [
-                'scalars' => [],
+                'scalars' => ['display_mode', 'full_section_image'],
                 'items' => [
                     'items' => ['icon', 'title', 'subtitle'],
                 ],
             ],
             'about' => [
-                'scalars' => ['image', 'kicker', 'title', 'text', 'button'],
+                'scalars' => ['display_mode', 'full_section_image', 'image', 'kicker', 'title', 'text', 'button'],
                 'items' => [
                     'profile_images' => ['image'],
                     'points' => ['icon', 'title', 'sub'],
@@ -28,56 +28,58 @@ class HomepageSectionSchema
                 ],
             ],
             'services' => [
-                'scalars' => ['kicker', 'title', 'text', 'more', 'button'],
+                'scalars' => ['display_mode', 'full_section_image', 'kicker', 'title', 'text', 'more', 'button'],
                 'items' => [
                     'items' => ['number', 'image', 'title', 'desc'],
                 ],
             ],
             'process' => [
-                'scalars' => ['kicker', 'title'],
+                'scalars' => ['display_mode', 'full_section_image', 'kicker', 'title'],
                 'items' => [
                     'items' => ['number', 'title', 'desc'],
                 ],
             ],
             'industries' => [
-                'scalars' => ['title', 'button'],
+                'scalars' => ['display_mode', 'full_section_image', 'title', 'button'],
                 'items' => [
                     'items' => ['image', 'label'],
                 ],
             ],
             'operations' => [
                 'scalars' => [
+                    'maintenance_display_mode', 'maintenance_full_section_image',
                     'maintenance_image', 'maintenance_title', 'maintenance_text', 'maintenance_button',
+                    'portal_display_mode', 'portal_full_section_image',
                     'portal_image', 'portal_title', 'portal_text', 'portal_button',
                 ],
                 'checks' => ['maintenance_checks', 'portal_checks'],
             ],
             'why' => [
-                'scalars' => ['title'],
+                'scalars' => ['display_mode', 'full_section_image', 'title'],
                 'items' => [
                     'items' => ['icon', 'title', 'desc'],
                 ],
             ],
             'showcase' => [
-                'scalars' => ['kicker', 'title', 'text', 'previous', 'next'],
+                'scalars' => ['display_mode', 'full_section_image', 'kicker', 'title', 'text', 'previous', 'next'],
                 'items' => [
                     'metrics' => ['icon', 'value', 'label'],
                 ],
             ],
             'clients' => [
-                'scalars' => ['title', 'text', 'more', 'button'],
+                'scalars' => ['display_mode', 'full_section_image', 'title', 'text', 'more', 'button'],
                 'items' => [
                     'logos' => ['image'],
                 ],
             ],
             'emergency' => [
-                'scalars' => ['title', 'text', 'button', 'contact', 'photo_alt', 'support', 'call', 'email'],
+                'scalars' => ['display_mode', 'full_section_image', 'title', 'text', 'button', 'contact', 'photo_alt', 'support', 'call', 'email'],
             ],
             'footer_cta' => [
-                'scalars' => ['title', 'text', 'quote', 'contact'],
+                'scalars' => ['display_mode', 'full_section_image', 'title', 'text', 'quote', 'contact'],
             ],
             'footer' => [
-                'scalars' => ['about', 'company', 'services_label', 'contact_label'],
+                'scalars' => ['display_mode', 'full_section_image', 'about', 'company', 'services_label', 'contact_label'],
                 'checks' => ['contact_lines'],
             ],
             default => ['scalars' => [], 'items' => []],
