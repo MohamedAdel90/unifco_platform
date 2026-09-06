8610bca40ebf12ad2e9e38c0f6313f31a481971c
MohamedAdel90 <adelwork90@gmail.com>
2026-09-06 12:16:34 +0300

    Enable spare parts request compatibility guard

--- routes/public.php
+++ routes/public.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\EAM\AssetSparePartController;
 use App\Http\Controllers\Maintenance\WorkOrderController;
 use App\Http\Controllers\PublicSiteController;
 use App\Http\Controllers\PublicSparePartsLookupController;
+use App\Http\Middleware\PublicSparePartsRequestCompatibility;
 use App\Http\Middleware\PublicSparePartsRequestPresentation;
 use Illuminate\Support\Facades\Route;
 
@@ -25,8 +26,8 @@ Route::get('/', [PublicSiteController::class, 'home'])->name('public.home');
 Route::get('/about', fn () => redirect()->route('public.home'))->name('public.about');
 Route::get('/industries', fn () => redirect()->route('public.home'))->name('public.industries');
 Route::get('/services', fn () => redirect()->route('public.home'))->name('public.services');
-Route::get('/request-quote', [PublicSiteController::class, 'quote'])->middleware(PublicSparePartsRequestPresentation::class)->name('public.quote');
-Route::get('/request-service', [PublicSiteController::class, 'quote'])->middleware(PublicSparePartsRequestPresentation::class)->name('public.request-service');
+Route::get('/request-quote', [PublicSiteController::class, 'quote'])->middleware([PublicSparePartsRequestCompatibility::class, PublicSparePartsRequestPresentation::class])->name('public.quote');
+Route::get('/request-service', [PublicSiteController::class, 'quote'])->middleware([PublicSparePartsRequestCompatibility::class, PublicSparePartsRequestPresentation::class])->name('public.request-service');
 Route::get('/emergency-maintenance', [PublicSiteController::class, 'emergency'])->name('public.emergency');
 Route::get('/asset-lookup', [PublicSiteController::class, 'assetLookup'])->middleware('throttle:60,1')->name('public.asset.lookup');
 Route::get('/spare-parts-lookup', PublicSparePartsLookupController::class)->middleware('throttle:60,1')->name('public.spare-parts.lookup');
