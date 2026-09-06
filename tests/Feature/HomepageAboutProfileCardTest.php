793e79bd47c4ed752a15c38bfd3f0cb94621e3af
MohamedAdel90 <adelwork90@gmail.com>
2026-09-06 12:55:55 +0300

    Align homepage About regression tests with current profile modal

--- tests/Feature/HomepageAboutProfileCardTest.php
+++ tests/Feature/HomepageAboutProfileCardTest.php
@@ -6,35 +6,31 @@ use Tests\TestCase;
 
 class HomepageAboutProfileCardTest extends TestCase
 {
-    public function test_homepage_about_action_links_to_about_route_in_both_locales(): void
+    public function test_homepage_about_action_opens_profile_modal_in_both_locales(): void
     {
         foreach (['ar', 'en'] as $locale) {
             $html = $this->get('/?lang='.$locale)->assertOk()->getContent();
 
-            $this->assertStringContainsString('/about?lang='.$locale, $html);
-            $this->assertStringNotContainsString('id="about-company-trigger"', $html);
+            $this->assertStringContainsString('id="about-company-trigger"', $html);
+            $this->assertStringContainsString('id="unifco-profile-modal"', $html);
+            $this->assertStringContainsString('id="unifco-profile-modal-script"', $html);
         }
     }
 
-    public function test_legacy_about_profile_modal_is_not_rendered_anymore(): void
+    public function test_homepage_about_profile_keeps_locale_specific_artwork(): void
     {
-        foreach (['ar', 'en'] as $locale) {
-            $html = $this->get('/?lang='.$locale)->assertOk()->getContent();
+        $ar = $this->get('/?lang=ar')->assertOk()->getContent();
+        $en = $this->get('/?lang=en')->assertOk()->getContent();
 
-            $this->assertStringNotContainsString('id="unifco-profile-modal"', $html);
-            $this->assertStringNotContainsString('id="unifco-profile-modal-script"', $html);
-            $this->assertStringNotContainsString('/images/home/unifco-about-card-en.webp', $html);
-            $this->assertStringNotContainsString('/images/home/unifco-about-card-ar.webp', $html);
-        }
+        $this->assertStringContainsString('/images/home/unifco-about-card-ar.webp', $ar);
+        $this->assertStringContainsString('/images/home/unifco-about-card-en.webp', $en);
     }
 
-    public function test_homepage_keeps_responsive_layout_without_legacy_profile_artwork_rules(): void
+    public function test_homepage_profile_modal_keeps_responsive_dialog_rules(): void
     {
         $html = $this->get('/?lang=ar')->assertOk()->getContent();
 
-        $this->assertStringNotContainsString('width:210vw', $html);
-        $this->assertStringNotContainsString('width:392vw', $html);
-        $this->assertStringNotContainsString('unifco-profile-dialog', $html);
-        $this->assertStringNotContainsString('unifco-profile-artwork', $html);
+        $this->assertStringContainsString('unifco-profile-dialog', $html);
+        $this->assertStringContainsString('unifco-profile-artwork', $html);
     }
 }
