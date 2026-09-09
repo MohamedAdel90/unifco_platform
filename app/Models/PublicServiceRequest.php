2a2bddf5716e729b8503d9ee0417a92d7e4a5ac4
MohamedAdel90 <adelwork90@gmail.com>
2026-09-09 14:59:39 +0300

    Support video attachments on public requests

--- app/Models/PublicServiceRequest.php
+++ app/Models/PublicServiceRequest.php
@@ -9,7 +9,7 @@ class PublicServiceRequest extends Model
     protected $fillable = [
         'reference_no','ticket_serial','request_type','request_intent','request_subtype','service_category','service_family','asset_type','service_other',
         'equipment_brand','equipment_model','subject','details','site_name','site_area','site_city','site_address','latitude','longitude','urgency',
-        'requested_date','requested_time','equipment_image_path','equipment_photo_paths','problem_photo_paths','supporting_image_paths','supporting_document_paths','previous_report_paths',
+        'requested_date','requested_time','equipment_image_path','equipment_photo_paths','problem_photo_paths','supporting_image_paths','supporting_document_paths','previous_report_paths','video_attachment_path',
         'company_name','responsible_person','contact_role','commercial_registration','email','mobile','status','submitted_at',
         'tenant_id','organization_id','asset_id','crm_lead_id','crm_opportunity_id','crm_quotation_id',
         'service_request_id','work_order_id','converted_at',
