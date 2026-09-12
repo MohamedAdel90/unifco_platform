03cdb5ac6aa5de18e2d645b332ce1aad0274cabb
MohamedAdel90 <adelwork90@gmail.com>
2026-09-13 02:40:47 +0300

    Support redesigned customer profile fields

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -10,9 +10,9 @@ class Customer extends Model
     use BelongsToTenant;
 
     protected $fillable = [
-        'tenant_id','organization_id','customer_code','name','commercial_registration','vat_number','industry',
-        'email','contact_name','contract_manager_name','contract_manager_title','project_name','logo_path',
-        'phone','city','country','address','status','onboarding_status','onboarding_review_status','onboarding_reviewed_by','onboarding_reviewed_at','onboarding_review_notes',
+        'tenant_id','organization_id','customer_code','name','name_ar','commercial_registration','vat_number','industry','customer_type',
+        'email','website','contact_name','contact_title','contract_manager_name','contract_manager_title','project_name','logo_path',
+        'phone','alternate_phone','city','country','address','notes','status','onboarding_status','onboarding_review_status','onboarding_reviewed_by','onboarding_reviewed_at','onboarding_review_notes',
         'acquisition_source','origin_lead_id','first_touch_at','converted_by','converted_at'
     ];
 
