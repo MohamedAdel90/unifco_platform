fde3064b991fba916a491033444a0a8d9243ce90
MohamedAdel90 <adelwork90@gmail.com>
2026-09-14 09:10:37 +0300

    Add request workflow workspace route

--- routes/field.php
+++ routes/field.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CustomerProfileController;
 use App\Http\Controllers\FieldServiceController;
 use App\Http\Controllers\Operations\{OperationsReportController,ProjectSiteOperationsController};
+use App\Http\Controllers\Workflow\MaintenanceRequestWorkflowController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
@@ -12,6 +13,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/operations-manager/reports',OperationsReportController::class)
         ->middleware('permission:reporting.executive.read')
         ->name('operations-manager.reports');
+    Route::get('/service-requests/{serviceRequest}/workflow',[MaintenanceRequestWorkflowController::class,'show'])
+        ->name('service-requests.workflow.show');
     Route::prefix('field')->name('field.')->group(function () {
         Route::get('/operations',[FieldServiceController::class,'operations'])->name('operations');
         Route::post('/assignments',[FieldServiceController::class,'assign'])->name('assign');
