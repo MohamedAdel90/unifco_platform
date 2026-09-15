c4f59f54b097b4672b19c3317666cff863c01c77
MohamedAdel90 <adelwork90@gmail.com>
2026-09-15 19:00:11 +0300

    Trigger dashboard V2.1 qualification after test repair

--- tests/Feature/CustomerPortalExecutiveDashboardTest.php
+++ tests/Feature/CustomerPortalExecutiveDashboardTest.php
@@ -44,5 +44,6 @@ class CustomerPortalExecutiveDashboardTest extends TestCase
 
         $this->assertStringContainsString('grid-template-columns:repeat(8,1fr)', $view);
         $this->assertStringContainsString("attention {{ \$pendingQ>0?'warn':'' }}", $view);
+        $this->assertStringContainsString('server-authorized scope', $view);
     }
 }
