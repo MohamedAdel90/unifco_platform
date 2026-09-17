e2318351e0ab22db3c4ad0975f5571de10684275
MohamedAdel90 <adelwork90@gmail.com>
2026-09-17 14:13:06 +0300

    Restore stable Visits & Schedule workspace route

--- resources/views/customer/section.blade.php
+++ resources/views/customer/section.blade.php
@@ -4,8 +4,6 @@
     @include('customer.partials.dashboard-sidebar-sync')
 @elseif($section === 'work-orders')
     @include('customer.work-orders')
-@elseif($section === 'visits')
-    @include('customer.visits')
 @elseif($section === 'maintenance')
     @include('customer.maintenance-plan')
 @elseif($section === 'spare-parts')
