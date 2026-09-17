b0321bb173fed0b06fb656c23edf01b35dad12cf
MohamedAdel90 <adelwork90@gmail.com>
2026-09-17 13:35:07 +0300

    Redeploy approved customer dashboard styling

--- resources/views/customer/section.blade.php
+++ resources/views/customer/section.blade.php
@@ -1,3 +1,4 @@
+{{-- Customer dashboard uses the approved synchronized sidebar and visual styling. --}}
 @if($section === 'dashboard')
     @include('customer.dashboard-v3')
     @include('customer.partials.dashboard-sidebar-sync')
